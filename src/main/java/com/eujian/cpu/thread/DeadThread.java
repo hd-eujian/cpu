e76382a59a15f5ed879166002270145e2eba8036
yeyongjian <yeyongjian@gac-nio.com>
2020-10-17 11:39:15 +0800

    1

--- src/main/java/com/eujian/cpu/thread/DeadThread.java
+++ src/main/java/com/eujian/cpu/thread/DeadThread.java
@@ -14,9 +14,11 @@ public class DeadThread {
     public void DeadFun(){
         for (int i =0;i<20;i++){
             Thread thread = new Thread(() -> {
+                int x =0;
                 while (true) {
+                    x++;
                     new DeadThread();
-                    log.info("创建DeadThread,");
+                    log.info("创建DeadThread-"+x);
                 }
             });
 
