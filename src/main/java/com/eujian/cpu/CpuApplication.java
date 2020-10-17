359704b0d5d5b2207d28c9bf65b9e9c713c5fcbb
yeyongjian <yeyongjian@gac-nio.com>
2020-10-17 11:06:45 +0800

    1

---
+++ src/main/java/com/eujian/cpu/CpuApplication.java
@@ -0,0 +1,13 @@
+package com.eujian.cpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpuApplication.class, args);
+    }
+
+}
